a280d95cfc2ca03ed17b10aebfd9c9b1cbe3fff4
Batudo <batudo@chrom.ar>
2025-05-24 13:39:34 -0300

    Stricter TS config

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
     <App />
   </ThemeProvider>,
