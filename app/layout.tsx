f596276497bef11dc24759db4580fbdbb82f3b33
Batudo <batudo@chrom.ar>
2025-06-12 12:42:10 -0300

    Open Graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Chrom.ar',
-  description: 'Chrom.ar',
+  description: 'Chrom.ar, on-chain workflows automation protocol',
   generator: 'Chrom.ar',
 }
 
@@ -14,6 +14,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:url" content="https://chrom.ar/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Chrom.ar" />
+        <meta property="og:description" content="Chrom.ar, on-chain workflows automation protocol." />
+        <meta property="og:image" content="https://chrom.ar/chroma.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="chrom.ar" />
+        <meta property="twitter:url" content="https://chrom.ar/" />
+        <meta name="twitter:title" content="Chrom.ar" />
+        <meta name="twitter:description" content="Chrom.ar, on-chain workflows automation protocol." />
+        <meta name="twitter:image" content="https://chrom.ar/chroma.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
