2fc674b91f0c52196a37fd9d9212b09377aae324
Batudo <batudo@chrom.ar>
2025-09-19 11:46:48 -0300

    ESLint _migration_

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,39 +11,38 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-export default [
-  ...compat.extends("next/core-web-vitals"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: "module",
-    },
-
-    rules: {
-      "@typescript-eslint/naming-convention": ["error", {
-        selector: "import",
-        format: ["camelCase", "PascalCase"],
-      }],
-
-      "arrow-parens": ["error", "as-needed"],
-      "comma-dangle": ["error", "always-multiline"],
-      "curly": "error",
-      "eqeqeq": "error",
-      "func-style": ["error", "expression"],
-      "indent": ["error", 2],
-      "key-spacing": ["error", { beforeColon: false, afterColon: true }],
-      "no-multiple-empty-lines": ["error", { max: 1 }],
-      "no-tabs": "error",
-      "no-throw-literal": "error",
-      "no-trailing-spaces": "error",
-      "quotes": ["error", "double"],
-      "semi": "error",
-    },
+export default [{
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}, ...compat.extends("next/core-web-vitals"), {
+  files: ["**/*.ts", "**/*.tsx"],
+  plugins: {
+    "@typescript-eslint": typescriptEslint,
   },
-];
+
+  languageOptions: {
+    parser: tsParser,
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+
+  rules: {
+    "@typescript-eslint/naming-convention": ["error", {
+      selector: "import",
+      format: ["camelCase", "PascalCase"],
+    }],
+
+    "arrow-parens": ["error", "as-needed"],
+    "comma-dangle": ["error", "always-multiline"],
+    "curly": "error",
+    "eqeqeq": "error",
+    "func-style": ["error", "expression"],
+    "indent": ["error", 2],
+    "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+    "no-multiple-empty-lines": ["error", { max: 1 }],
+    "no-tabs": "error",
+    "no-throw-literal": "error",
+    "no-trailing-spaces": "error",
+    "quotes": ["error", "double"],
+    "semi": "error",
+  },
+}];
