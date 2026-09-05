021cecef26d8fcd752a0900b90862e934abd4d3a
Batudo <batudo@chrom.ar>
2026-09-05 14:51:16 -0300

    Point absolute URLs at vora.chrom.ar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://chrom.ar",
+  site: "https://vora.chrom.ar",
   build: { format: "directory" },
   i18n: {
     defaultLocale: "en",
